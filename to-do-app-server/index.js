e4e75a44dba0b5e92e2fcffa2cf985ea48b9c7d7
Mohammad Rafe <mrrafi5869@gmail.com>
2023-10-12 17:08:59 +0600

    table

--- to-do-app-server/index.js
+++ to-do-app-server/index.js
@@ -1,13 +1,56 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9t60goe.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const taskCollection = client.db('toDoApp').collection('taskList');
+
+    app.get("/taskList", async(req, res) => {
+        const query = {};
+        const taskList = await taskCollection.find(query).toArray();
+        res.send(taskList);
+    });
+
+    app.post("/postTask", async(req, res) => {
+        const task = req.body;
+        const postTask = await taskCollection.insertOne(task);
+        res.send(postTask);
+    });
+    app.delete("/deleteTask", async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)}
+        const deleteTask = await taskCollection.deleteOne(query)
+        res.send(deleteTask);
+    });
+
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
 app.get("/", async(req, res) => {
     res.send("to do app server is running");
-})
+});
 
 app.listen(port, () => console.log("server is running"));
